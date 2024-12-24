e72d72e77b0dd018aa83ad1c09ad7344656bb7cf
pr0mis3 <94976593+p3ris0n@users.noreply.github.com>
2024-12-24 17:31:20 +0100

    still a portfolio.

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -42,5 +42,11 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return <div>
+    <div className="container">
+      <p>Real-World Results</p>
+      <h2>Featured Projects</h2>
+      <p>See how I transformed concepts.</p>
+    </div>
+  </div>;
 };
