bcf50026a5cc33b5c8f3f7cb859b83698e43a354
pr0mis3 <94976593+p3ris0n@users.noreply.github.com>
2024-12-25 17:05:08 +0100

    Implemented a tape-like banner for an about-me type thing.

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import CheckCircleIcon from '@/assets/icons/check-circle.svg';
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 import grainImage from '@/assets/images/grain.jpg';
+
 const portfolioProjects = [
   {
     company: "Acme Corp",
@@ -54,38 +55,44 @@ export const ProjectsSection = () => {
       <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">See how I transformed concepts.</p>
       <div className="mt-10 md:mt-20 flex flex-col gap-20">
         {portfolioProjects.map((project) => (
-          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded text-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none">
+          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl text-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none">
             <div className="absolute inset-0 -z-10 opacity-5" style={{
               backgroundImage: `url(${grainImage.src})`
             }}></div>
-            <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-              <span>{project.company}</span>
-              <span>&bull;</span>
-              <span>{project.year}</span>
+            <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+              <div className="lg:pb-16">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                  <span>{project.company}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
+                <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+                <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
+                <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                  {project.results.map((result) => (
+                    <li className="flex gap-2 text-sm md:text-base text-white/50" key={result.title}>
+                      <CheckCircleIcon className="size-5 md:size-6" />
+                      <span>{result.title}</span>
+                    </li>
+                  ))}
+                </ul>
+                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                  <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold text-sm inline-flex items-center justify-center gap-2 mt-8" aria-label={`View Live Site</button ${project.title}`}>
+                    <span>
+                      View Live Site
+                    </span>
+                    <ArrowUpRightIcon className="size-4" />
+                  </button>
+                </a>
+              </div>
+              <div className="relative">
+                <Image
+                    src={project.image}
+                    alt={`Screenshot of ${project.title}`}
+                    className="mt-8 -mb-5 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
+              </div>
             </div>
-            <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
-            <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
-            <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-              {project.results.map((result) => (
-                <li className="flex gap-2 text-sm md:text-base text-white/50" key={result.title}>
-                  <CheckCircleIcon className="size-5 md:size-6" />
-                  <span>{result.title}</span>
-                </li>
-              ))}
-            </ul>
-            <a href={project.link} target="_blank" rel="noopener noreferrer">
-              <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold text-sm inline-flex items-center justify-center gap-2 mt-8" aria-label={`View Live Site</button ${project.title}`}>
-                <span>
-                  View Live Site
-                </span>
-                <ArrowUpRightIcon className="size-4" />
-              </button>
-            </a>
-            <Image
-                src={project.image}
-                alt={`Screenshot of ${project.title}`}
-                className="mt-8 -mb-5 md:-mb-0"
-              />
           </div>
         ))}
       </div>
